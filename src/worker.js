e73b85a81c8abbdfdc4ad0d04a920017eea85ea6
Ekaterina <katujxa19881@gmail.com>
2025-09-23 10:33:22 +0200

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -109,6 +109,7 @@ if (!state.classes) state.classes = {};
     bells_caption: null,
     bus_file_id: null,
     bus_caption: null,
+    
 // три независимых набора "времён"
     pickup_times: null, // уроки (основное)
     aftercare_times: null, // продлёнка / ГПД
