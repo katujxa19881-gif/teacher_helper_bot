5af9b4d30bca61152b7d4c44a1255a597ff62f09
Ekaterina <katujxa19881@gmail.com>
2025-09-23 10:55:49 +0200

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -97,10 +97,11 @@ async function saveState(env, state) { await env.KV_BOT.put("state", JSON.string
 
 ffunction ensureClass(state, cls) {
     if (!state.classes) state.classes = {};
-
-// значения по умолчанию
-const defaults = {
-// привязки чатов
+  
+  // значения по умолчанию
+  const defaults = {
+    
+    // привязки чатов
     general_chat_id: null,
 
 // медиа: расписание уроков, звонков, автобус
